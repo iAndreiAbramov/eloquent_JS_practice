36c4758e9d856a01153e100cfea6ae2d25266dd9
Andrei <i.andrei.abramov@gmail.com>
2021-03-08 22:31:36 +0300

    recursive_arr_spreader_complete

--- 0_spread_array.js
+++ 0_spread_array.js
@@ -3,24 +3,12 @@
 function arrSpread(arr) {
   if (!Array.isArray(arr)) return null;
 
-  let result = [];
-
-  function reducer(arr) {
-    arr.reduce((arr, elem) => {
-      if (!Array.isArray(elem)) {
-        result.concat(elem);
-      } else {
-        result.concat(elem.reduce(reducer(arr, elem)));
-      }
-    });
-  }
-
-  result = reducer(arr);
+  let result = arr.reduce((acc, elem) => acc.concat((Array.isArray(elem)) ? arrSpread(elem) : elem), []);
 
   return result;
 }
 
-console.log(arrSpread([1, [1, 3, [0, 0, [1, 1, 1], 0], 4], 2, 3, [3, 4, 5, [5, 6, 7]]]));
+// console.log(arrSpread([1, [1, 3, [0, 0, [1, 1, 1], 0], 4], 2, 3, [3, 4, 5, [5, 6, 7]]]));
 
 console.log(arrSpread([1, [], 2, [0, 0, [9, [5, 5, 5], 9, 9], 0], 3, [3, 4, [1, 1, 1], 5, [5, 6, 7]]]));
 
